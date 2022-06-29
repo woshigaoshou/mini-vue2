7bf58cd7679dd2eabe0dfd1e59cdd9e8c0800a6e
woshigaoshou <1258006989@qq.com>
2022-06-30 00:58:54 +0800

    feat: 添加data、computed、props等选项的合并策略

--- src/util/options.js
+++ src/util/options.js
@@ -1,3 +1,5 @@
+import { extend } from './util';
+
 const strats = {};
 const LIFTCYCLE_HOOKS = [
   'beforeCreate',
@@ -14,10 +16,22 @@ LIFTCYCLE_HOOKS.forEach(hook => {
   strats[hook] = mergeHook;
 });
 
+// 对象类型的option，采用的是extend方式，即先完成parentVal的赋值，之后取childVal的key直接对res[key]进行覆盖
+strats.props =
+strats.methods =
+strats.inject =
+strats.computed = function (parentVal, childVal) {
+  if (!parentVal) return childVal
+  const ret = Object.create(null)
+  extend(ret, parentVal)
+  if (childVal) extend(ret, childVal)
+  return ret
+}
+
 function mergeHook (parentVal, childVal) {
   if (childVal) {
     if (parentVal) {
-      parentVal.concat(childVal);
+      return parentVal.concat(childVal);
     } else {
       return [childVal];
     }
@@ -26,9 +40,45 @@ function mergeHook (parentVal, childVal) {
   }
 }
 
-export function mergeOptions (parent, child) {
+strats.data = function (parentVal, childVal, vm) {
+  console.log(parentVal, childVal);
+  
+  return mergeData(
+    typeof parentVal === 'function' ? parentVal.call(vm || this) : parentVal,
+    typeof childVal === 'function' ? childVal.call(vm || this) : childVal,
+  )
+}
+
+function mergeData (to = {}, from) {
+  if (!from) return to;
+  const keys = Object.keys(from);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    console.log(key, to, from);
+    
+    to[key] = from[key];
+  }
+  return to;
+}
+
+const defaultStrat = function (parentVal, childVal) {
+  return childVal || parentVal;
+}
+
+export function mergeOptions (parent, child, vm) {
+  console.log(parent, child);
+  
   // parent优先级大于child
   const options = {};
+  // 若options存在mixins(嵌套)，则优先合并options.mixins
+  // 所以最终组件权重 > 组件内mixins(由于是for循环向前遍历，多个mixins，越靠后权重越高) > mixins内的mixins > 全局mixin > 全局mixin嵌套的mixin
+  if (child.mixins) {
+    for (let i = 0; i < child.mixins.length; i++) {
+      console.log('aaa',  child.mixins[i]);
+      
+      parent = mergeOptions(parent, child.mixins[i]);
+    }
+  }
   for (let k in parent) {
     mergeField(k);
   }
@@ -38,11 +88,8 @@ export function mergeOptions (parent, child) {
     }
   }
   function mergeField (k) {
-    if (strats[k]) {
-      options[k] = strats[k](parent[k], child[k]);
-    } else {
-      options[k] = child[k] || parent[k];
-    }
+    const strat = strats[k] || defaultStrat;
+    options[k] = strat(parent[k], child[k], vm);
   }
   return options;
 }
